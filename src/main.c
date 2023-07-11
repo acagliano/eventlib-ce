f39d057d62ba0826048d3f0aaa0b6b1daeee5152
Anthony Cagliano <acagliano97@gmail.com>
2023-07-10 22:29:45 -0400

    push more code revisions

--- src/main.c
+++ src/main.c
@@ -6,150 +6,181 @@
 #include <string.h>
 #include <time.h>
 
-// EV FLAGS
-#define EV_PERSISTENT	0
-#define EV_TIMEOUT_IS_INTERVAL	1
+#include <ti/getkey.h>
+#include <stdio.h>
+
+
+
 struct ev {
+	bool slot_used;
 	uint24_t event_id;
 	bool enabled, triggered;
-	uint24_t (*callback)(void*, size_t);
+	void (*callback)(void*, size_t);
 	size_t callback_data_len;
-	uint8_t callback_data[];
+	void* callback_data;
+	uint8_t ev_flags;
 };
 
-
+#define CEMU_CONSOLE ((char*)0xFB0000)
 
 typedef enum {
-	EV_OK,
 	EV_INVALID_ARG,
-	EV_ALLOC_THRESHOLD_EXCEEDED,
+	EV_THRESHOLD_EXCEEDED,
 	EV_ALLOC_FAILURE,
 	EV_DATABUF_OVERFLOW
 } ev_status_t;
 
-uint8_t ev_running = 0, ev_write = 0;
-void* (*ev_realloc)(void*, size_t) = NULL;
-size_t ev_max_alloc = 0;
-size_t ev_alloc_current = 0;
-uint8_t *ev_queue;
+void* (*ev_malloc)(size_t) = NULL;
+void (*ev_free)(void*) = NULL;
+
+#define EV_SLOTS_MAX 256
+struct ev ev_queue[EV_SLOTS_MAX];
 
 
-ev_status_t ev_setup(size_t max_alloc, void* (*realloc)(void*, size_t)){
-	if(max_alloc == 0) return EV_INVALID_ARG;
-	if(realloc == NULL) return EV_INVALID_ARG;
-	ev_max_alloc = max_alloc;
-	ev_realloc = realloc;
-	return EV_OK;
+bool ev_Setup(void* (*malloc)(size_t), void (*free)(void*)){
+	if(malloc == NULL || free==NULL) return false;
+	ev_malloc = malloc;
+	ev_free = free;
+	return true;
 }
 
-ev_status_t ev_register(uint24_t event_id,
-						uint24_t (*callback)(void*, size_t),
+enum _ev_flags {
+	EV_AUTOENABLE_WATCHER	= 0,
+	EV_DISABLE_AFTER_RUN	= 1,
+};
+
+int ev_RegisterEvent(uint8_t event_id,
+						uint8_t ev_flags,
+						void (*callback)(void*, size_t),
 						void *callback_data, size_t callback_data_len){
 	
-	if(callback == NULL) return EV_INVALID_ARG;
+	if(callback == NULL) return -1;
 	
-	uint8_t *ptr_start = ev_queue;
-	uint8_t *ptr_end = ev_queue + ev_alloc_current;
-	struct ev* ev_this = NULL;
+	int i = 0;
 	
-	for(uint8_t* curr = ptr_start; curr < ptr_end;){
-		struct ev *ev_tmp = (struct ev*)curr;
+	void* data = ev_malloc(callback_data_len);
+	if(data==NULL) return -1;
+	memcpy(data, callback_data, callback_data_len);
+	
+	for(; i < EV_SLOTS_MAX; i++)
+		if(!ev_queue[i].slot_used) break;
+	
+	if(i == EV_SLOTS_MAX) return -1;
 		
-		if(ev_tmp->event_id == event_id){
-			if(ev_tmp->callback_data_len < callback_data_len)
-				return EV_DATABUF_OVERFLOW;
-			ev_this = ev_tmp;
-			break;
+	struct ev *this = &ev_queue[i];
+	
+	this->event_id = event_id;
+	this->callback = callback;
+	this->callback_data_len = callback_data_len;
+	this->callback_data = data;
+	this->ev_flags = ev_flags;
+	this->slot_used = true;
+	
+	if(ev_flags & (1 << EV_AUTOENABLE_WATCHER)) this->enabled = true;
+
+	return i;
+}
+
+bool ev_UnregisterEvent(int slot){
+	
+	if(slot >= EV_SLOTS_MAX) return false;
+	struct ev *this = &ev_queue[slot];
+	
+	ev_free(this->callback_data);
+	memset(this, 0, sizeof(struct ev));
+	return true;
+}
+
+bool ev_UpdateCallbacks(int slot, void (*callback)(void*, size_t), void* callback_data, size_t callback_data_len, void* (*realloc)(void*, size_t)){
+	
+	if(slot >= EV_SLOTS_MAX) return false;
+	
+	struct ev *this = &ev_queue[slot];
+	if(!this->slot_used) return false;
+	
+	if(callback) this->callback = callback;
+	if(callback_data && callback_data_len){
+		if(this->callback_data_len != callback_data_len){
+			uint8_t *ptr = realloc(this->callback_data, callback_data_len);
+			if(!ptr) return false;
+			this->callback_data = ptr;
+			this->callback_data_len = callback_data_len;
 		}
-		curr += (sizeof(struct ev) + ev_tmp->callback_data_len);
+		memcpy(this->callback_data, callback_data, callback_data_len);
 	}
 	
-	if(ev_this == NULL){
-		size_t alloc_this = sizeof(struct ev) + callback_data_len;
-		if((ev_alloc_current + alloc_this) > ev_max_alloc) return EV_ALLOC_THRESHOLD_EXCEEDED;
-		
-		uint8_t *ptr = ev_realloc(ev_queue, ev_alloc_current + alloc_this);
-		if(ptr == NULL) return EV_ALLOC_FAILURE;
-		
-		ev_queue = ptr;
-		
-		ev_this = (struct ev*)ev_queue + ev_alloc_current;
-		ev_alloc_current += alloc_this;
-		memset(ev_this, 0, sizeof(struct ev) + callback_data_len);
-	}
-	
-	ev_this->event_id = event_id;
-	ev_this->callback = callback;
-	ev_this->callback_data_len = callback_data_len;
-	memcpy(ev_this->callback_data, callback_data, callback_data_len);
-
-	return EV_OK;
+	return true;
 }
 
-ev_status_t ev_enable(uint8_t event_id){
+void ev_PurgeEvent(uint8_t event_id, uint8_t num){
+	size_t ct = 0;
 	
-	uint8_t *ptr_start = ev_queue;
-	uint8_t *ptr_end = ev_queue + ev_alloc_current;
-	
-	for(uint8_t* curr = ptr_start; curr < ptr_end;){
-		struct ev *ev_this = (struct ev*)curr;
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
 		
-		if(ev_this->event_id == event_id)
-		   ev_this->enabled = true;
-		
-		curr += (sizeof(struct ev) + ev_this->callback_data_len);
+		if(this->event_id == event_id){
+			ev_free(this->callback_data);
+			memset(this, 0, sizeof(struct ev));
+			if(ct++ == num) break;
+		}
 	}
-	return EV_OK;
 }
 
-ev_status_t ev_disable(uint8_t event_id){
+void ev_Watch(uint8_t event_id){
 	
-	uint8_t *ptr_start = ev_queue;
-	uint8_t *ptr_end = ev_queue + ev_alloc_current;
-	
-	for(uint8_t* curr = ptr_start; curr < ptr_end;){
-		struct ev *ev_this = (struct ev*)curr;
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
 		
-		if(ev_this->event_id == event_id)
-		   ev_this->enabled = false;
-		
-		curr += (sizeof(struct ev) + ev_this->callback_data_len);
+		if(this->event_id == event_id)
+			this->enabled = true;
 	}
-	return EV_OK;
+}
+
+void ev_Unwatch(uint8_t event_id){
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
+		
+		if(this->event_id == event_id)
+			this->enabled = true;
+	}
 }
 		   
-ev_status_t ev_trigger(uint8_t event_id){
-	uint8_t *ptr_start = ev_queue;
-	uint8_t *ptr_end = ev_queue + ev_alloc_current;
-	
-	for(uint8_t* curr = ptr_start; curr < ptr_end;){
-		struct ev *ev_this = (struct ev*)curr;
+void ev_Trigger(uint8_t event_id){
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
 		
-		if(ev_this->event_id == event_id)
-			ev_this->triggered = true;
-		
-		curr += (sizeof(struct ev) + ev_this->callback_data_len);
+		if((this->event_id == event_id) && this->enabled)
+			this->triggered = true;
 	}
-	return EV_OK;
 }
 
-ev_status_t ev_check(){
+void ev_HandleEvents(void){
 	
-	uint8_t *ptr_start = ev_queue;
-	uint8_t *ptr_end = ev_queue + ev_alloc_current;
-	
-	for(uint8_t* curr = ptr_start; curr < ptr_end;){
-		struct ev *ev_this = (struct ev*)curr;
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
 		
-		if(ev_this->enabled && ev_this->triggered)
-			ev_this->callback(ev_this->callback_data, ev_this->callback_data_len);
-		
-		curr += (sizeof(struct ev) + ev_this->callback_data_len);
+		if(this->enabled && this->triggered){
+			this->callback(this->callback_data, this->callback_data_len);
+			if(this->ev_flags & (1 << EV_DISABLE_AFTER_RUN)) this->enabled = false;
+		}
 	}
-	return EV_OK;
 }
 
+void ev_Cleanup(void){
+	for(int i = 0; i < EV_SLOTS_MAX; i++){
+		if(!ev_queue[i].slot_used) continue;
+		struct ev *this = &ev_queue[i];
+		
+		ev_free(this->callback_data);
+	}
+}
 
+#define EV0 0
+#define EV1 1
 int main(void){
-	
 }
